b5747009f6a2f47b817a553c673468fdde1ab3cf
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-04-29 12:34:11 +0430

    initial things made

---
+++ sketch.js
@@ -0,0 +1,51 @@
+particles = [];
+
+function setup() {
+    createCanvas(window.innerWidth - 1000, window.innerHeight - 25);
+}
+
+function draw() {
+    background(0);
+    //controllable, quantity of particles
+    for (let i = 0; i < 1; i++) {
+        let p = new Particle();
+        particles.push(p);
+    }
+    for (let i = particles.length - 1; i >= 0; i--) {
+        particles[i].update();
+        particles[i].show();
+        if (particles[i].finished()) {
+            // remove this particle
+            particles.splice(i, 1);
+        }
+    }
+}
+
+class Particle {
+
+    constructor() {
+        this.x = 300;
+        this.y = 30;
+        //velocity of particles controllable
+        this.vx = random(-1, 1);
+        this.vy = random(+5, -1);
+        this.alpha = 255;
+    }
+
+    finished() {
+        return this.alpha < 0;
+    }
+
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+        this.alpha -= 1;
+    }
+
+    show() {
+        noStroke();
+        // make the colors random
+        fill(255, 89, 38, this.alpha);
+        ellipse(this.x, this.y, 5);
+    }
+}
